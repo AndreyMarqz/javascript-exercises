bca51fa4926ad31099e368be2cb9184ae92735e3
Andrey Marques <andreymmarques6@gmail.com>
2025-11-22 20:42:24 -0300

    Finished exercises from 06 to 11, about arrays methods with functions and logical thinking.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  conversion = (fahrenheit - 32) * 5/9;
+  conversion.toFixed(1);
+  const result = Math.round(conversion * 10) / 10;
+
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  conversion = (celsius * 9/5) + 32;
+  conversion.toFixed(1);
+  const result = Math.round(conversion * 10) / 10;
+
+  return result;
 };
 
 // Do not edit below this line
