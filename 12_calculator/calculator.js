ed313c80c2acb60d825715ce2d4b9ec10c71a5a2
Andrey Marques <andreymmarques6@gmail.com>
2025-12-09 23:01:58 -0300

    Finished the objects array exercises from 12 to 16

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+  return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((current, next) => current + next, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((current, next) => current * next, 1);
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+	let result = 1;
+
+  for (let i = 0; i < n2; i++){
+    result *= n1;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = 1;
+
+  for (let i = n; i <= n && i != 0; i--){
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    let num = parseInt(n);
 
+  if (num < 0) {
+    return "OOPS";
+  } else if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  }
+
+  let a = 0; // Represents F(i-2)
+  let b = 1; // Represents F(i-1)
+  let nextFib;
+
+  for (let i = 2; i <= num; i++) {
+    nextFib = a + b;
+    a = b;
+    b = nextFib;
+  }
+  return nextFib;
 };
 
 // Do not edit below this line
